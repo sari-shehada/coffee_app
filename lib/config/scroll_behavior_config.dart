6b00539a2937676d88bf93e4d4fe1c85c59fc22e
Sari Shehada <sarishehada@gmail.com>
2025-03-09 01:10:57 +0400

    Added LightTheme & ScrollBahavior

---
+++ lib/config/scroll_behavior_config.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ScrollBehaviorConfig extends ScrollBehavior {
+  const ScrollBehaviorConfig();
+
+  @override
+  Widget buildOverscrollIndicator(
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
+    return child;
+  }
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
+  }
+}
---
+++ lib/config/themes/light_theme.dart
@@ -0,0 +1,44 @@
+import 'package:coffee_app/config/themes/theme_constants.dart';
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  fontFamily: fontFamily,
+  splashColor: Colors.transparent,
+  colorScheme: ColorScheme(
+    brightness: Brightness.light,
+    primary: _brownNormal,
+    primaryContainer: _brownLight,
+    onPrimary: _surfaceWhite,
+    onSurface: _greyNormalActive,
+    onSurfaceVariant: _greyNormalHover,
+    outline: _greyLightHover,
+    outlineVariant: _greyLighter,
+    surfaceContainerLow: _surfaceLightActive,
+    surface: _surfaceLight,
+    surfaceContainerHighest: _surfaceWhite,
+    secondary: _brownNormal,
+    onSecondary: _surfaceWhite,
+    error: Colors.red,
+    onError: _surfaceWhite,
+  ),
+);
+
+//Colors as constants as represented in the Figma design file
+//Foundation/Brown/Normal
+const Color _brownNormal = Color(0xFFC67C4E);
+//Foundation/Brown/Light
+const Color _brownLight = Color(0xFFF9F2ED);
+//Foundation/Grey/Normal :active
+const Color _greyNormalActive = Color(0xFF242424);
+//Foundation/Grey/Normal :hover
+const Color _greyNormalHover = Color(0xFF2A2A2A);
+//Foundation/Grey/Light :hover
+const Color _greyLightHover = Color(0xFF909090);
+//Foundation/Grey/Lighter
+const Color _greyLighter = Color(0xFFA2A2A2);
+//Foundation/Surface/Light :active
+const Color _surfaceLightActive = Color(0xFFE3E3E3);
+//Foundation/Surface/Light :active
+const Color _surfaceLight = Color(0xFFF9F9F9);
+//Foundation/Surface/White
+const Color _surfaceWhite = Color(0xFFFFFFFF);
