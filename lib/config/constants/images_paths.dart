a5ee51d3ffbfb38cfccdb0719009c20cb5c855e8
Sari Shehada <sarishehada@gmail.com>
2025-03-16 01:58:45 +0400

    Add new asset: promo-image

--- lib/config/constants/images_paths.dart
+++ lib/config/constants/images_paths.dart
@@ -4,3 +4,4 @@ const String kImagesPath = '${kAssetsPath}images/';
 const String kProductsPath = '${kImagesPath}products/';
 
 const String kLandingImagePath = '${kImagesPath}landing.png';
+const String kPromoImagePath = '${kImagesPath}promo-image.png';
