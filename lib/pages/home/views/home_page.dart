727cb7be91f928c58071d2c1334032c1c9a4fa61
Sari Shehada <sarishehada@gmail.com>
2025-04-06 20:34:28 +0400

    Tabbar now pins to screen while scrolling

--- lib/pages/home/views/home_page.dart
+++ lib/pages/home/views/home_page.dart
@@ -1,11 +1,10 @@
-import 'package:coffee_app/core/extensions/list_extensions.dart';
 import 'package:coffee_app/core/extensions/responsive_extensions.dart';
 import 'package:coffee_app/core/utils/ui/spacing/spacing_utils.dart';
 import 'package:coffee_app/pages/home/controllers/home_controller.dart';
 import 'package:coffee_app/pages/home/views/widgets/header/home_location_text_widget.dart';
 import 'package:coffee_app/pages/home/views/widgets/header/home_search_and_filter_widget.dart';
 import 'package:coffee_app/pages/home/views/widgets/promo/home_promo_widget.dart';
-import 'package:coffee_app/pages/home/views/widgets/tab_bar/home_page_tab_bar_item.dart';
+import 'package:coffee_app/pages/home/views/widgets/tab_bar/home_page_tabbar_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
 
@@ -28,45 +27,25 @@ class HomePage extends GetView<HomeController> {
             ),
             pinned: true,
           ),
-          SliverFillRemaining(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 24.fromHeight,
-                    bottom: 16.fromHeight,
-                  ),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: TabBar(
-                      padding: EdgeInsetsDirectional.only(end: 24.fromWidth),
-                      labelPadding: EdgeInsetsDirectional.only(
-                        start: 24.fromWidth,
-                      ),
-                      splashFactory: NoSplash.splashFactory,
-                      isScrollable: true,
-                      tabAlignment: TabAlignment.start,
-                      controller: controller.tabController,
-                      dividerHeight: 0,
-                      indicatorColor: Colors.transparent,
-                      tabs:
-                          controller.tabBarTabLabels
-                              .mapWithIndex(
-                                (item, itemIndex) => Obx(
-                                  () => HomePageTabBarItem(
-                                    label: item,
-                                    isSelected:
-                                        itemIndex ==
-                                        controller.currentIndex.value,
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                    ),
-                  ),
-                ),
-              ],
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: HomePageTabbarDelegate(
+              tabController: controller.tabController,
+              tabLabels: controller.tabBarTabLabels,
+              currentTabIndex: controller.currentIndex,
             ),
+          ),
+          SliverList.list(
+            children: List.generate(20, (index) {
+              return Opacity(
+                opacity: .12,
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  color: index.isEven ? Colors.amber : Colors.amberAccent,
+                ),
+              );
+            }),
           ),
         ],
       ),
---
+++ lib/pages/home/views/widgets/tab_bar/home_page_tabbar_delegate.dart
@@ -0,0 +1,84 @@
+import 'package:coffee_app/core/extensions/color_extensions.dart';
+import 'package:coffee_app/core/extensions/list_extensions.dart';
+import 'package:coffee_app/core/extensions/responsive_extensions.dart';
+import 'package:coffee_app/pages/home/views/widgets/tab_bar/home_page_tab_bar_item.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//TODO find another implementation that doesnt enforce
+//setting a height constraint on the tabbar (to avoid reponsiveness issues)
+class HomePageTabbarDelegate extends SliverPersistentHeaderDelegate {
+  const HomePageTabbarDelegate({
+    required this.tabController,
+    required this.tabLabels,
+    required this.currentTabIndex,
+  });
+
+  final RxInt currentTabIndex;
+  final TabController tabController;
+  final List<String> tabLabels;
+
+  @override
+  double get maxExtent => 71;
+
+  @override
+  double get minExtent => 70;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final ThemeData themeData = Theme.of(context);
+    final ColorScheme colorScheme = themeData.colorScheme;
+    return AnimatedContainer(
+      decoration: BoxDecoration(
+        color: colorScheme.surfaceContainerHighest,
+        boxShadow: [
+          if (overlapsContent) ...[
+            BoxShadow(
+              offset: Offset(0, 8),
+              color: colorScheme.shadow.applyOpacity(.09),
+              blurRadius: 12,
+            ),
+          ],
+        ],
+      ),
+      height: 71,
+      curve: Easing.legacy,
+      duration: 300.milliseconds,
+      child: Padding(
+        padding: EdgeInsets.only(top: 24.fromHeight, bottom: 16.fromHeight),
+        child: SizedBox(
+          width: double.infinity,
+          child: TabBar(
+            padding: EdgeInsetsDirectional.only(end: 24.fromWidth),
+            labelPadding: EdgeInsetsDirectional.only(start: 24.fromWidth),
+            splashFactory: NoSplash.splashFactory,
+            isScrollable: true,
+            tabAlignment: TabAlignment.start,
+            controller: tabController,
+            dividerHeight: 0,
+            indicatorColor: Colors.transparent,
+            tabs:
+                tabLabels
+                    .mapWithIndex(
+                      (item, itemIndex) => Obx(
+                        () => HomePageTabBarItem(
+                          label: item,
+                          isSelected: itemIndex == currentTabIndex.value,
+                        ),
+                      ),
+                    )
+                    .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
